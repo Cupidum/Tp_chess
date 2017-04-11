89fb6b1dc9a2f4f0bb1e3427f645b00d0ef0ef11
Cupidum <jeremiasraymundo@hotmail.com>
2017-04-10 21:24:38 -0400

    changed style

--- src/Piece.java
+++ src/Piece.java
@@ -1,17 +1,16 @@
-public abstract class Piece
-{
-        protected Type piece;
+public abstract class Piece {
+    protected Type piece;
     
-        public enum Type{
-            WHITE, BLACK, ROI, REINE, FOU, CHEVAL, TOUR, PION
-        }
+    public enum Type {
+		WHITE, BLACK, ROI, REINE, FOU, CHEVAL, TOUR, PION
+    }
 	
 	
-	public Piece(Type type){
-            this.piece = type;
-        }
+	public Piece(Type type) {
+        this.piece = type;
+    }
 	
-	public Type getType(){
-            return piece;
-        }	
+	public Type getType() {
+        return piece;
+    }	
 }
