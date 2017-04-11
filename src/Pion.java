bced11aae6fe6632afdc84bb38431c43e50554df
Cupidum <jeremiasraymundo@hotmail.com>
2017-04-10 22:32:36 -0400

    indentation fixed

--- src/Pion.java
+++ src/Pion.java
@@ -1,17 +1,17 @@
 public class Pion {
-	private PieceType type;
-	private Couleur couleur;
+    private PieceType type;
+    private Couleur couleur;
 	
-	public Pion(PieceType type, Couleur couleur) {
+    public Pion(PieceType type, Couleur couleur) {
         this.type = type;
         this.couleur = couleur;
-	}
+    }
 	
-	public PieceType getType() {
+    public PieceType getType() {
         return type;
-	}
+    }
 	
-	public Couleur getCouleur() {
+    public Couleur getCouleur() {
         return couleur;
-	}
+    }
 }
