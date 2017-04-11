1191d33b6ecc065394b0cc18589baf25e6964a6f
Cupidum <jeremiasraymundo@hotmail.com>
2017-04-10 22:26:44 -0400

    added class Pion

---
+++ src/Pion.java
@@ -0,0 +1,17 @@
+public class Pion {
+	private PieceType type;
+	private Couleur couleur;
+	
+	public Pion(PieceType type, Couleur couleur) {
+		this.type = type;
+		this.couleur = couleur;
+	}
+	
+	public PieceType getType() {
+		return type;
+	}
+	
+	public Couleur getCouleur() {
+		return couleur;
+	}
+}
