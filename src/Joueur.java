41da0fa4cc076c1269140f6441e5e04870eab7ef
Cupidum <jeremiasraymundo@hotmail.com>
2017-04-10 23:04:20 -0400

    Added Joueur.java

---
+++ src/Joueur.java
@@ -0,0 +1,11 @@
+public class Joueur() {
+    private Couleur couleur;
+    
+    public 	Joueur(Couleur couleur) {
+	   	this.couleur = couleur;
+    }
+	
+	public Couleur getCouleur() {
+	    return couleur;
+	}	
+}
