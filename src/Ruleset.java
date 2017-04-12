af2103ee7e9199f2883aaac4a9655a56cd372662
Cupidum <jeremiasraymundo@hotmail.com>
2017-04-12 19:52:34 -0400

    ruleRoi modified

--- src/Ruleset.java
+++ src/Ruleset.java
@@ -99,60 +99,72 @@ public class Ruleset {
     }
 	
 	public void ruleRoi() {
-	    List<Square> allowed = new ArrayList<>();
+        List<Square> allowed = new ArrayList<>();
         Square from = move.getFrom();
         Square to = move.getTo();
-    
+
         Square current;
-		
+
         // Right
-        current = board[i][from.getY()];
+        current = board.getSquare(from.getX, from.getY());
         if (!current.isEmpty()) {
-            if (move.getPiece().getColor() != current.getPiece().getColor()) {
+            if (move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
                 allowed.add(current);
             }
             break;
-            }
-        allowed.add(current);    
         }
-    
+        allowed.add(current);    
+    }
+
         // Left
-        current = board[i][from.getY()];
-        if (!current.isEmpty()) {
-            if (move.getPiece().getColor() != current.getPiece().getColor()) {
-                allowed.add(current);
-            }
-            break;
-        }
-        allowed.add(current);   
     
-        // Above
-        current = board[from.getX()][i];
+        current = board.getSquare(from.getX, from.getY());
         if (!current.isEmpty()) {
-            if (move.getPiece().getColor() != current.getPiece().getColor()) {
-                allowed.add(current);
-            }
-            break;
-        }
-        allowed.add(current);    
-     
-    
-        // Below
-        current = board[from.getX()][i];
-        if (!current.isEmpty()) {
-            if (move.getPiece().getColor() != current.getPiece().getColor()) {
+            if (move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
                 allowed.add(current);
             }
             break;
         }
         allowed.add(current);
+    }
+
+        // Above
+        current = board.getSquare(from.getX(), from.getY);
+        if (!current.isEmpty()) {
+            if (move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                allowed.add(current);
+            }
+            break;
         }
-    
+        allowed.add(current);
+    }
+
+        // Below
+        current = board.getSquare(from.getX(), from.getY);
+        if (!current.isEmpty()) {
+            if (move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                allowed.add(current);
+            }
+            break;
+        }
+        allowed.add(current);
+    }
+
         if (!allowed.contains(to)) {
             // ERROR
         } else {
-            board[to.getX()][to.getY()].setPiece(move.getPiece());
-                board[from.getX()][to.getY()].setPiece(null);
+            board.setPiece(move.getPiece(), to.getX(), to.getY());
+            board.setPiece(null, from.getX(), from.getX());
         }
-	}
+    }
+	public void ruleCheval() {
+        List<Square> allowed = new ArrayList<>();
+        Square from = move.getFrom();
+        Square to = move.getTo();
+    
+        Square current;
+		
+		//up right
+		if(move.)
+    }
 }
