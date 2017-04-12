96019b4262241460f7fde9cf4ec4b40d2369286b
Cupidum <jeremiasraymundo@hotmail.com>
2017-04-11 22:00:58 -0400

    added method

--- src/Ruleset.java
+++ src/Ruleset.java
@@ -1,3 +1,20 @@
 public class Ruleset {
+
+    private Board[][] board;
+    private Move move;
+
+    public Ruleset(Board[][] board, Move move) {
+        this.board = board;
+        this.move = move;		
+    }
 	
+    public void rulePion() {
+        if(move.getPiece().getCouleur() == BLANC) {
+            if(move.getTo().getX() - move.getfrom().getX() == 0 && move.getTo().getY() - move.getfrom().getY() == 1)
+                board[x][move.getTo()];
+        }
+        else
+            if(move.getTo().getX() - move.getfrom().getX() == 0 && move.getTo().getY() - move.getfrom().getY() == -1)
+                board[x][move.getTo()];
+    }
 }
