1f10920de2b8ed22d964520af5df55b9a291bbb2
Cupidum <jeremiasraymundo@hotmail.com>
2017-04-12 01:37:32 -0400

    modified class ruleTour

--- src/Ruleset.java
+++ src/Ruleset.java
@@ -33,26 +33,65 @@ public class Ruleset {
 }
 	
     public void ruleTour(Move move) {
+    List<Square> allowed = new ArrayList<>();
     Square from = move.getFrom();
     Square to = move.getTo();
     
-    Square allowed = null;
-    if (move.getPiece().getColor() == Color.BLANC) {
-        allowed = board[from.getX()][from.getY() + 1];
-    } else {
-        allowed = board[from.getX()][from.getY() - 1];
+    Square current;
+    
+    // Right
+    for (int i = from.getX(); i < 8; i++) {
+        current = board[i][from.getY()];
+        if (!current.isEmpty()) {
+            if (move.getPiece().getColor() != current.getPiece().getColor()) {
+                allowed.add(current);
+            }
+            break;
+        }
+        allowed.add(current);    
     }
     
-    if (allowed.getX() != to.getX() && allowed.getY() != to.getY()) {
-        // ERROR
+    // Left
+    for (int i = from.getX(); i >=0; i--) {
+        current = board[i][from.getY()];
+        if (!current.isEmpty()) {
+            if (move.getPiece().getColor() != current.getPiece().getColor()) {
+                allowed.add(current);
+            }
+            break;
+        }
+        allowed.add(current)    
     }
     
-    // Il y a une piece dans la position possible
-    else if (!allowed.isEmpty()) {
+    // Above
+    for (int i = from.getY(); i < 8; i++) {
+        current = board[from.getX()][i];
+        if (!current.isEmpty()) {
+            if (move.getPiece().getColor() != current.getPiece().getColor()) {
+                allowed.add(current);
+            }
+            break;
+        }
+        allowed.add(current)    
+    }
+    
+    // Below
+    for (int i = from.getY(); i >= 0; i--) {
+        current = board[from.getX()][i];
+        if (!current.isEmpty()) {
+            if (move.getPiece().getColor() != current.getPiece().getColor()) {
+                allowed.add(current);
+            }
+            break;
+        }
+        allowed.add(current)
+    }
+    
+    if (!allowed.contains(to)) {
         // ERROR
     } else {
         board[to.getX()][to.getY()].setPiece(move.getPiece());
-        board[from.getX()][to.getY()].setPiece(null);
-    }        
+            board[from.getX()][to.getY()].setPiece(null);
+    }
 }
 }
