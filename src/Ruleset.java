f73d2fb23925e463e144d55e097073866ba324c6
Cupidum <jeremiasraymundo@hotmail.com>
2017-04-12 22:12:34 -0400

    finished it

--- src/Ruleset.java
+++ src/Ruleset.java
@@ -104,7 +104,51 @@ public class Ruleset {
         Square to = move.getTo();
 
         Square current;
-
+		
+        //up right
+        if (from.getX() == 1 && from.getY() == 1) {
+            current = board.getSquare(from.getX(), from.getY());
+            if (!current.isEmpty()) {
+                if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                    allowed.add(current);
+                }
+            }
+        allowed.add(current);    
+        }
+		
+        //up left
+        if (from.getX() == -1 && from.getY() == 1) {
+            current = board.getSquare(from.getX(), from.getY());
+            if (!current.isEmpty()) {
+                if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                    allowed.add(current);
+                }
+            }
+        allowed.add(current);    
+        }
+		
+		//down left
+        if (from.getX() == -1 && from.getY() == -1) {
+            current = board.getSquare(from.getX(), from.getY());
+            if (!current.isEmpty()) {
+                if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                    allowed.add(current);
+                }
+            }
+        allowed.add(current);    
+        }
+		
+		//down right
+        if (from.getX() == 1 && from.getY() == -1) {
+            current = board.getSquare(from.getX(), from.getY());
+            if (!current.isEmpty()) {
+                if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                    allowed.add(current);
+                }
+            }
+        allowed.add(current);    
+        }
+		
         // Right
         current = board.getSquare(from.getX(), from.getY());
         if (!current.isEmpty()) {
@@ -148,7 +192,7 @@ public class Ruleset {
             board.setPiece(null, from.getX(), from.getX());
         }
     }
-}
+
     public void ruleCheval() {
         List<Square> allowed = new ArrayList<>();
         Square from = move.getFrom();
@@ -315,6 +359,125 @@ public class Ruleset {
             }
         }
         
+        if (!allowed.contains(to)) {
+            // ERROR
+        } else {
+            board.setPiece(move.getPiece(), to.getX(), to.getY());
+            board.setPiece(null, from.getX(), from.getX());
+        }		
+    }
+	
+    public void ruleReine() {
+        List<Square> allowed = new ArrayList<>();
+        Square from = move.getFrom();
+        Square to = move.getTo();
+    
+        Square current;
+		
+		//up right
+        for(int i = from.getX(); i < 8; i++) {
+            for(int j = from.getY(); j < 8; j++) {
+                current = board.getSquare(from.getX(), from.getY());
+                if (!current.isEmpty()) {
+                    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                        allowed.add(current);
+                    }
+                    break;
+                }
+                allowed.add(current);
+            }
+        }
+
+        //up left
+        for(int i = from.getX(); i >= 0; i--) {
+            for(int j = from.getY(); j < 8; j++) {
+                current = board.getSquare(from.getX(), from.getY());
+                if (!current.isEmpty()) {
+                    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                        allowed.add(current);
+                    }
+                    break;
+                }
+                allowed.add(current);
+            }
+        }
+
+        //down left
+        for(int i = from.getX(); i >= 0; i--) {
+            for(int j = from.getY(); j >= 0; j--) {
+                current = board.getSquare(from.getX(), from.getY());
+                if (!current.isEmpty()) {
+                    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                        allowed.add(current);
+                    }
+                    break;
+                }
+                allowed.add(current);
+            }
+        }
+
+        //down right
+        for(int i = from.getX(); i < 8; i++) {
+            for(int j = from.getY(); j >= 0; j--) {
+                current = board.getSquare(from.getX(), from.getY());
+                if (!current.isEmpty()) {
+                    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                        allowed.add(current);
+                    }
+                    break;
+                }
+                allowed.add(current);
+            }
+        }
+        
+		// Right
+        for (int i = from.getX(); i < 8; i++) {
+            current = board.getSquare(i, from.getY());
+            if (!current.isEmpty()) {
+                if (move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                    allowed.add(current);
+                }
+                break;
+            }
+            allowed.add(current);    
+        }
+
+        // Left
+        for (int i = from.getX(); i >=0; i--) {
+            current = board.getSquare(i, from.getY());
+            if (!current.isEmpty()) {
+                if (move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                    allowed.add(current);
+                }
+                break;
+            }
+            allowed.add(current);
+        }
+
+        // Above
+        for (int i = from.getY(); i < 8; i++) {
+            current = board.getSquare(from.getX(), i);
+            if (!current.isEmpty()) {
+                if (move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                    allowed.add(current);
+                }
+                break;
+            }
+            allowed.add(current);
+        }
+
+        // Below
+        for (int i = from.getY(); i >= 0; i--) {
+            current = board.getSquare(from.getX(), i);
+            if (!current.isEmpty()) {
+                if (move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                    allowed.add(current);
+                }
+                break;
+            }
+            allowed.add(current);
+        }
+		
         if (!allowed.contains(to)) {
             // ERROR
         } else {
