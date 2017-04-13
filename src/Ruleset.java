3367626c8ab4ef231908ead4fee78c8fd4dae1c4
Cupidum <jeremiasraymundo@hotmail.com>
2017-04-12 21:49:15 -0400

    fixed indentation and ruleFou added

--- src/Ruleset.java
+++ src/Ruleset.java
@@ -149,7 +149,7 @@ public class Ruleset {
         }
     }
 }
-	public void ruleCheval() {
+    public void ruleCheval() {
         List<Square> allowed = new ArrayList<>();
         Square from = move.getFrom();
         Square to = move.getTo();
@@ -157,91 +157,169 @@ public class Ruleset {
         Square current;
 		
 		//up right
-		if (from.getX() == 1 && from.getY() == 2) {
+        if (from.getX() == 1 && from.getY() == 2) {
             current = board.getSquare(from.getX(), from.getY());
-			if (!current.isEmpty()) {
-			    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+            if (!current.isEmpty()) {
+                if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
                     allowed.add(current);
-				}
+                }
             }
-        allowed.add(current)    
+        allowed.add(current);    
         }
         
         //up left
         if (from.getX() == -1 && from.getY() == 2) {
             current = board.getSquare(from.getX(), from.getY());
-			if (!current.isEmpty()) {
-			    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+            if (!current.isEmpty()) {
+                if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
                     allowed.add(current);
-				}
+                }
             }
-        allowed.add(current)    
+        allowed.add(current);    
         }
 
         //left up
         if (from.getX() == -2 && from.getY() == 1) {
             current = board.getSquare(from.getX(), from.getY());
-			if (!current.isEmpty()) {
-			    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+            if (!current.isEmpty()) {
+                if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
                     allowed.add(current);
-				}
+                }
             }
-        allowed.add(current)    
+        allowed.add(current);    
         }
 
         //left down
         if (from.getX() == -2 && from.getY() == -1) {
             current = board.getSquare(from.getX(), from.getY());
-			if (!current.isEmpty()) {
-			    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+            if (!current.isEmpty()) {
+                if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
                     allowed.add(current);
-				}
+                }
             }
-        allowed.add(current)    
+        allowed.add(current);    
         }
 
         //down left
         if (from.getX() == -1 && from.getY() == -2) {
             current = board.getSquare(from.getX(), from.getY());
-			if (!current.isEmpty()) {
-			    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+            if (!current.isEmpty()) {
+                if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
                     allowed.add(current);
-				}
+                }
             }
-        allowed.add(current)    
+        allowed.add(current);    
         }
 
         //down right
         if (from.getX() == 1 && from.getY() == -2) {
             current = board.getSquare(from.getX(), from.getY());
 			if (!current.isEmpty()) {
-			    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
                     allowed.add(current);
-				}
+                }
             }
-        allowed.add(current)    
+        allowed.add(current);    
         }
 
         //right down
         if (from.getX() == 2 && from.getY() == -1) {
             current = board.getSquare(from.getX(), from.getY());
-			if (!current.isEmpty()) {
-			    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+            if (!current.isEmpty()) {
+                if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
                     allowed.add(current);
-				}
+                }
             }
-        allowed.add(current)    
+        allowed.add(current);    
         }
 
         //right up
         if (from.getX() == 2 && from.getY() == 1) {
             current = board.getSquare(from.getX(), from.getY());
-			if (!current.isEmpty()) {
-			    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+            if (!current.isEmpty()) {
+                if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
                     allowed.add(current);
-				}
+                }
             }
-        allowed.add(current)    
+        allowed.add(current);    
+        }
+        
+        if (!allowed.contains(to)) {
+            // ERROR
+        } else {
+            board.setPiece(move.getPiece(), to.getX(), to.getY());
+            board.setPiece(null, from.getX(), from.getX());
+        }		
+    }
+	
+	public void ruleFou() {
+		List<Square> allowed = new ArrayList<>();
+        Square from = move.getFrom();
+        Square to = move.getTo();
+    
+        Square current;
+		
+		//up right
+        for(int i = from.getX(); i < 8; i++) {
+            for(int j = from.getY(); j < 8; j++) {
+                current = board.getSquare(from.getX(), from.getY());
+                if (!current.isEmpty()) {
+                    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                        allowed.add(current);
+                    }
+                    break;
+                }
+                allowed.add(current);
+            }
+        }
+
+        //up left
+        for(int i = from.getX(); i >= 0; i--) {
+            for(int j = from.getY(); j < 8; j++) {
+                current = board.getSquare(from.getX(), from.getY());
+                if (!current.isEmpty()) {
+                    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                        allowed.add(current);
+                    }
+                    break;
+                }
+                allowed.add(current);
+            }
+        }
+
+        //down left
+        for(int i = from.getX(); i >= 0; i--) {
+            for(int j = from.getY(); j >= 0; j--) {
+                current = board.getSquare(from.getX(), from.getY());
+                if (!current.isEmpty()) {
+                    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                        allowed.add(current);
+                    }
+                    break;
+                }
+                allowed.add(current);
+            }
+        }
+
+        //down right
+        for(int i = from.getX(); i < 8; i++) {
+            for(int j = from.getY(); j >= 0; j--) {
+                current = board.getSquare(from.getX(), from.getY());
+                if (!current.isEmpty()) {
+                    if(move.getPiece().getCouleur() != current.getPiece().getCouleur()) {
+                        allowed.add(current);
+                    }
+                    break;
+                }
+                allowed.add(current);
+            }
+        }
+        
+        if (!allowed.contains(to)) {
+            // ERROR
+        } else {
+            board.setPiece(move.getPiece(), to.getX(), to.getY());
+            board.setPiece(null, from.getX(), from.getX());
         }		
     }
 }
